5cf0ab902e09eb7d87990a1f78b19ded1b479a9a
Mike Frampton <michael.frampton@pg.canterbury.ac.nz>
2016-09-20 15:39:33 +1200

    the scons script now gets all the image dependencies so it will rebuild the latex file whenever an image changes
    Still haven't added any dependencies to the image build itself
    since that's not handled by scons yet. In the future I might just
    add the script as an automatic dep of the image.

--- script/imgdep.py
+++ script/imgdep.py
@@ -16,12 +16,23 @@ class ImgArgs:
             key += '_' + self.args
         return key
 
+    def get_script_path(self):
+        return os.path.join(_get_script_dir(), self.script)
+
+    def get_dest_path(self):
+        return os.path.join(_get_dest_dir(), self.img)
+
+
 # a dictionary mapping of image filename to script (including
-# script arguments)
+# script arguments) for all dependencies known in the imgdep.json file
 fname_to_script = None
 
 # a set of already processed scripts
 processed_scripts = set()
+
+# all image dependencies for this script (an ImgArgs object for each image
+# which called img()
+imgdeps = {}
 
 def _get_dest_dir():
     scriptdir = _get_script_dir()
@@ -63,26 +74,36 @@ def _load_image_deps():
             imgargs = ImgArgs(scriptpath, args, img)
             fname_to_script[img] = imgargs
 
-def _process_img(imgpath):
+def _get_img_args(imgpath):
     if not fname_to_script.has_key(imgpath):
         raise Exception(
             'Image {} was not found in imgdeps file.'.format(imgpath))
 
-    destpath = os.path.join(_get_dest_dir(), imgpath)
-    if os.path.isfile(destpath):
-        print('=== img already exists, skipping: {}'.format(imgpath))
-        return
-
     imgargs = fname_to_script[imgpath]
+    scriptfull = imgargs.get_script_path()
+    if not os.path.isfile(scriptfull):
+        raise Exception(
+            'Script does not exist {}'.format(scriptfull))
+    return imgargs
+
+def _add_img_dep(imgpath):
+    imgargs = _get_img_args(imgpath)
+    if imgargs.img not in imgdeps:
+        imgdeps[imgargs.img] = imgargs
+
+def _process_img(imgpath):
+
+    imgargs = _get_img_args(imgpath)
     args = ''
     if imgargs.args is not None:
         args = imgargs.args
 
-    scriptrel = imgargs.script
-    scriptfull = os.path.join(_get_script_dir(), scriptrel)
-    if not os.path.isfile(scriptfull):
-        raise Exception(
-            'Script does not exist {}'.format(scriptfull))
+    destpath = imgargs.get_dest_path()
+    if os.path.isfile(destpath):
+        print('=== img already exists, skipping: {}'.format(imgpath))
+        return
+
+    scriptfull = imgargs.get_script_path()
 
     global processed_scripts
     if not imgargs.processed_key() in processed_scripts:
@@ -97,16 +118,23 @@ def _process_img(imgpath):
 
         processed_scripts.add(imgargs.processed_key())
     else:
-        print('=== script already run, skipping {}'.format(scriptrel))
+        print('=== script already run, skipping {}'.format(imgargs.script))
 
     if not os.path.isfile(destpath):
         raise Exception(
             'Image {} was not built by script {}.'
             .format(imgpath, scriptfull))
 
+def get_img_deps():
+    img = []
+    for imgarg in imgdeps.values():
+        img.append(imgarg.get_script_path())
+    return img
+
 def img(imgpath):
     if fname_to_script is None:
         _load_image_deps()
 
+    _add_img_dep(imgpath)
     _process_img(imgpath)
 
